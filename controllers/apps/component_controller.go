79834e6496a712bff0df9b19247c7e56b09de7a9
谢金虎 <1050780355@qq.com>
2026-08-21 15:30:21 +0800

    fix: prune resource-derived config params on resize

--- controllers/apps/component_controller.go
+++ controllers/apps/component_controller.go
@@ -101,6 +101,7 @@ type ComponentReconciler struct {
 // +kubebuilder:rbac:groups=storage.k8s.io,resources=storageclasses,verbs=get;list;watch
 
 // +kubebuilder:rbac:groups=apps.kubeblocks.io,resources=componentresourceconstraints,verbs=get;list;watch
+// +kubebuilder:rbac:groups=apps.kubeblocks.io,resources=configconstraints,verbs=get;list;watch
 // +kubebuilder:rbac:groups=apps.kubeblocks.io,resources=opsrequests,verbs=get;list;watch;create
 
 // +kubebuilder:rbac:groups=core,resources=serviceaccounts,verbs=get;list;watch
